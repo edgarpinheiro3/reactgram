9010a0271d02c73aca34d4d66069c42817ab1941
Edgar Pinheiro <edgar.melo.prestador@prf.gov.br>
2024-02-16 09:25:26 -0300

    Slice do login

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -31,6 +31,22 @@ export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 });
 
+// Sign in an user
+export const login = createAsyncThunk("auth/login",
+  async(user, thunkAPI) => {
+    
+    const data = await authService.login(user);
+
+    // check for errors
+    if(data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+
+  }
+);
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -42,7 +58,8 @@ export const authSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(register.pending, (state) => {
+    builder
+    .addCase(register.pending, (state) => {
       state.loading = true;
       state.error = false;
     })
@@ -61,6 +78,21 @@ export const authSlice = createSlice({
       state.loading = false;
       state.success = true;
       state.error = null;
+      state.user = null;
+    })
+    .addCase(login.pending, (state) => {
+      state.loading = true;
+      state.error = false;
+    })
+    .addCase(login.fulfilled, (state, action) => {
+      state.loading = false;
+      state.success = true;
+      state.error = null;
+      state.user = action.payload;
+    })
+    .addCase(login.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
       state.user = null;
     });
   },
