c70cea48bc60861228e2f3c82c83b70d9204ec1e
Edgar Pinheiro <edgar.melo.prestador@prf.gov.br>
2024-02-19 11:09:41 -0300

    Service de atualização de usuário

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -3,7 +3,6 @@ import {api, requestConfig} from '../utils/config';
 //Get user details
 const profile = async(data, token) => {
   const config = requestConfig("GET", data, token);
-
   try {
 
     const res = await fetch(api + "/users/profile", config)
@@ -15,11 +14,27 @@ const profile = async(data, token) => {
   } catch (error) {
     console.log(error);
   }
+};
 
+// Update user details
+const updateProfile = async(data, token) => {
+
+  const config =  requestConfig("PUT", data, token, true);
+
+  try {
+    const res = await fetch(api + "/users/", config)
+                .then((res) => res.json())
+                .then((err) => err);
+    
+    return res;    
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const userService = {
   profile,
+  updateProfile,
 };
 
 export default userService;
