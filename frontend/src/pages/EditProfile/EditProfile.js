5224a57702ccde01f23d9e74c7e450c2b503d7f2
Edgar Pinheiro <edgar.melo.prestador@prf.gov.br>
2024-02-19 10:24:12 -0300

    Preenchendo formulário de edição com dados do usuário

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -1,6 +1,46 @@
 import "./EditProfile.css";
 
+import { uploads } from "../../utils/config";
+
+//Hooks
+import { useEffect, useState } from "react";
+import { useSelector,useDispatch } from "react-redux";
+
+//Redux
+import { profile, resetMessage } from "../../slices/userSlice";
+
+//Components
+import Message from "../../components/Message";
+
+
 const EditProfile = () => {
+
+  const dispatch = useDispatch();
+
+  const {user, message, error, loading} = useSelector((state) => state.user);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [bio, setBio] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  // Load user data
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
+
+  // Fill form with user data
+  useEffect(() => {
+
+    if(user) {
+      setName(user.name);
+      setEmail(user.email);
+      setBio(user.bio);
+    }
+
+  }, [user]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,11 +55,14 @@ const EditProfile = () => {
       <input 
           type="text" 
           placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""}
         />
         <input 
           type="email" 
           placeholder="E-mail"
           disabled
+          value={email || ""}
         />
         <label>
           <span>Imagem do Perfil:</span>
@@ -32,6 +75,8 @@ const EditProfile = () => {
           <input
             type="text"
             placeholder="Descrição do perfil"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio || ""}
           />
         </label>
         <label>
@@ -39,6 +84,8 @@ const EditProfile = () => {
           <input
             type="text"
             placeholder="Digite sua nova senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password || ""}
           />
         </label>
         <input type="submit" value="Atualizar" />
